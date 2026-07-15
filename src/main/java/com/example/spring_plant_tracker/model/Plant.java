34d578e1b79a6be2272e5610c4a2421306a4a0c8
ashleyd3391 <ashleyd3391@gmail.com>
2026-07-15 12:09:44 -0500

    Completed PlantModel

--- src/main/java/com/example/spring_plant_tracker/model/Plant.java
+++ src/main/java/com/example/spring_plant_tracker/model/Plant.java
@@ -1,5 +1,34 @@
 package com.example.spring_plant_tracker.model;
+import java.time.LocalDate;
 
 public class Plant {
-    
+    private Long id;
+    private String name;
+    private LocalDate lastWateredDate;
+
+
+    public Plant(String name, LocalDate lastWateredDate) {
+        this.name = name;
+        this.lastWateredDate = lastWateredDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public LocalDate getLastWateredDate(){
+        return lastWateredDate;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setLastWateredDate(LocalDate lastWateredDate){
+        this.lastWateredDate = lastWateredDate;
+    }
 }
