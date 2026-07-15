2a7299acba8ea708eb732d638abdd90c5c048e4b
ashleyd3391 <ashleyd3391@gmail.com>
2026-07-15 13:19:37 -0500

    Added Get and Post endpoints

--- src/main/java/com/example/spring_plant_tracker/controller/PlantController.java
+++ src/main/java/com/example/spring_plant_tracker/controller/PlantController.java
@@ -1,8 +1,27 @@
 package com.example.spring_plant_tracker.controller;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spring_plant_tracker.model.Plant;
+import com.example.spring_plant_tracker.service.PlantService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/v1")
 public class PlantController {
-    
+    private final PlantService plantService;
+
+    public PlantController(PlantService plantService) {
+        this.plantService = plantService;
+    }
+
+    @GetMapping("/plants")
+    public List<Plant> getPlants(){
+        return plantService.getAllPlants();
+    }
+
+    @PostMapping("/plants")
+    public Plant addPlant(@RequestBody Plant plant){
+        return plantService.addPlant(plant.getName(), plant.getLastWateredDate());
+    }
 }
 
