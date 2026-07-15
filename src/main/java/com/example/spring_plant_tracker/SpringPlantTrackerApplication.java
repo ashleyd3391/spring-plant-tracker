2b36ec9453f8b566235af2bfa63d31acb6a66595
ashleyd3391 <ashleyd3391@gmail.com>
2026-07-15 11:31:46 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/spring_plant_tracker/SpringPlantTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_plant_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlantTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlantTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_plant_tracker/SpringPlantTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_plant_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlantTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
